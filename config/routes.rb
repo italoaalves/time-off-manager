3578b687c9c3000a68cc9347e159f64a40d84241
Italo <italo.alves.jp@gmail.com>
2023-03-07 02:40:22 -0300

    setup: sidekiq and redis

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  devise_for :users
+
+  authenticate :user, -> (u) { u.admin? } do
+    mount Sidekiq::Web => '/admin/sidekiq'
+  end
+
   root 'vacations#index'
   resources :vacations do
     resources :off_times
   end
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
