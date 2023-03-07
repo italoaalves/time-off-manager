144aeede7f6586a0d071d92ee433dac705a865bb
Italo <italo.alves.jp@gmail.com>
2023-03-07 15:12:25 -0300

    fix: rollback seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,7 @@ User.create!(
     is_on_vacation: false
 )
 
+Vacation.create!(year: "2023", user: employee, balance: 30, expires_at: 1.year.after)
 vac = Vacation.create!(year: "2022", user: employee, balance: 30, expires_at: employee.starting_date.change(year: Date.today.year.to_i))
 
 off = vac.off_times.build(starts_at: Date.tomorrow - 6.months - 23.days, ends_at: Date.today - 6.months)
