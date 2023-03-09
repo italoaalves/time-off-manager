9b0ac3e6937d796d97664227a83de8755b883130
Italo <italo.alves.jp@gmail.com>
2023-03-09 11:31:43 -0300

    add: user info hint to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 employee = User.create!(
     name: "Jonh Doe",
     email: "jonhd@acme.com",
@@ -36,3 +29,12 @@ if off.save(validate: false)
 else
 	puts off.errors.messages
 end
+
+puts <<~WARN
+
+---------------------------------------------------------------
+
+  PLEASE CHECK THE seeds.rb FILE for USER SIGN IN INFO.
+
+---------------------------------------------------------------
+WARN
